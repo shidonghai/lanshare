2c5b1ba77b282fb47cfbbbbbeffada81be2d0338
KingBright <jl.kingbright@gmail.com>
2013-04-15 01:13:03 +0800

    on item long click

--- src/com/nano/lanshare/conn/ui/ConnectActivity.java
+++ src/com/nano/lanshare/conn/ui/ConnectActivity.java
@@ -1,6 +1,5 @@
 package com.nano.lanshare.conn.ui;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
