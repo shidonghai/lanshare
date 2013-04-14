594cb2ee146cfe19f9292339f8eeeccec6d0f9ed
KingBright <jl.kingbright@gmail.com>
2013-04-15 01:24:09 +0800

    long touch event

--- src/com/nano/lanshare/common/DragView.java
+++ src/com/nano/lanshare/common/DragView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 /**
@@ -14,25 +15,33 @@ import android.view.View;
  */
 public class DragView extends View {
 
-	private static int ALPHA = 128;
+	private static final int ALPHA = 166;
 	private Bitmap mDrawingCache;
 
 	private DragObject mDragObject;
 
-	private int left, top;
 	private Paint paint;
+	private Rect mSrc;
+	private Rect mDest;
+
+	private int widthHalf, heightHalf;
 
 	public DragView(Context context, Bitmap drawingCache) {
 		super(context);
 		mDrawingCache = drawingCache;
 		paint = new Paint();
 		paint.setAlpha(ALPHA);
+		mSrc = new Rect(0, 0, mDrawingCache.getWidth(),
+				mDrawingCache.getHeight());
+		mDest = new Rect(0, 0, 0, 0);
+		widthHalf = mDrawingCache.getWidth() * 3 / 4;
+		heightHalf = mDrawingCache.getHeight() * 3 / 4;
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		canvas.drawBitmap(mDrawingCache, left, top, paint);
+		canvas.drawBitmap(mDrawingCache, mSrc, mDest, paint);
 	}
 
 	public DragObject getDragObject() {
@@ -48,8 +57,10 @@ public class DragView extends View {
 	}
 
 	public void move(int x, int y) {
-		left = x;
-		top = y;
+		mDest.left = x - widthHalf;
+		mDest.right = x + widthHalf;
+		mDest.top = y - heightHalf;
+		mDest.bottom = y + heightHalf;
 		invalidate();
 	}
 }
